53eea3794dba65bf38f622b49a4b89a1d193491d
yamadapc <tacla.yamada@gmail.com>
2014-05-12 20:15:51 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+exports(method)
+---------------
+Takes a function which acts on a context object, and returns a function which
+takes the context as its first argument.
+
+Install with:
+```
+npm install deoopfy
+```
+
+And use like:
+```javascript
+var slice = deoopfy(Array.prototype.slice);
+slice([1, 2, 3], [1]) // => [2, 3]
+```
---
+++ test/deoopfy.test.js
@@ -0,0 +1,14 @@
+'use strict';
+var test = require('tap').test;
+var deoopfy = require('..');
+
+test('deoopfy', function(t) {
+  t.test('makes methods take the context as an argument', function(t) {
+    var slice = deoopfy(Array.prototype.slice);
+    var sliceRet = slice([1, 2, 3, 4], [2, 3]);
+
+    t.deepEqual(sliceRet, [3], 'slice([1, 2, 3, 4], [2, 3]) => [3]');
+    t.end();
+  });
+  t.end();
+});
