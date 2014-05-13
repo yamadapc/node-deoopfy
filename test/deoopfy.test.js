090d0f8e0f08efef6560c930eb18463d66633143
yamadapc <tacla.yamada@gmail.com>
2014-05-12 21:06:11 -0300

    Change the api to use trailing arguments.

--- test/deoopfy.test.js
+++ test/deoopfy.test.js
@@ -5,9 +5,9 @@ var deoopfy = require('..');
 test('deoopfy', function(t) {
   t.test('makes methods take the context as an argument', function(t) {
     var slice = deoopfy(Array.prototype.slice);
-    var sliceRet = slice([1, 2, 3, 4], [2, 3]);
+    var sliceRet = slice([1, 2, 3, 4], 2, 3);
 
-    t.deepEqual(sliceRet, [3], 'slice([1, 2, 3, 4], [2, 3]) => [3]');
+    t.deepEqual(sliceRet, [3], 'slice([1, 2, 3, 4], 2, 3) => [3]');
     t.end();
   });
   t.end();
